5d63edf71256e70e8db80badd3ad9fb8f1268ef7
ChocoTacoツ <kashika_hettihewa@Chocos-MacBook-Air.local>
2025-02-19 09:45:05 +0530

    testapp5

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+const expres = require('express');
+const dotenv =  require ('dotenv').config()
+const port = 5000
+
+const app = expres()
+
+app.listen(port,() => console.log(`Server started on port ${port}`))
